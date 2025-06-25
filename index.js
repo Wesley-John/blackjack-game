ccf8af5ce4acfee59abc4e291c82980ce92a8937
Wesley-John <wesleyjohn691@gmail.com>
2025-06-25 08:22:15 +0300

    Refined the conditional and did more practice.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card.")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game.")
+    isAlive = false
 }
+
+console.log(hasBlackJack)
