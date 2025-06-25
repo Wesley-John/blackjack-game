186fb956882841bd18ae204e63ead95da5de85dd
Wesley-John <wesleyjohn691@gmail.com>
2025-06-25 09:18:12 +0300

    Added button that displays the message to the user.

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card."
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game."
-    isAlive = false
-}
+let messageEl = document.getElementById("message-el")
 
-console.log(message)
+function startGame() {
+    if (sum <= 20) {
+    message = "Do you want to draw a new card."
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game."
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
