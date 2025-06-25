1f8e87354ef64416a14be8b5816d1b1fce6e63d5
Wesley-John <wesleyjohn691@gmail.com>
2025-06-25 13:48:11 +0300

    Modified the startGame() function. Did practice on && and || operators.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let sumEl = document.getElementById("sum-el")
@@ -24,6 +22,12 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    hasBlackJack = false
     renderGame()
 }
 
--- practice/practice.js
+++ practice/practice.js
@@ -286,3 +286,29 @@ function rollDice() {
     let faceValue = Math.floor(Math.random() * 6) + 1
     return faceValue
 }
+
+let hasSolvedChallenge = false
+let hasHintsLeft = false
+
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution()
+}
+
+function showSolution() {
+    console.log("Showing the solution....")
+}
+
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+let likesDocumentaries = true
+let likesStartups = false
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
+}
+
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}
