a2bad9b2336d02c801ac8af22e15025247d844ee
Wesley-John <wesleyjohn691@gmail.com>
2025-06-25 07:54:47 +0300

    Inital commit. Did some practice.

---
+++ practice/practice.js
@@ -0,0 +1,105 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 3
+let secondCard = 11
+
+let sum = firstCard + secondCard
+
+// Create a person object that contains three keys: name, age, and county.
+// Use yourself as an example to set the values for name, age, and country
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+
+// Call the logData() function to verify that it works
+
+let person = {
+    name: "John",
+    age: 18,
+    country: "Kenya"
+}
+
+function logData() {
+    let dataString = person.name + " is " + person.age + " years old and lives in " + person.country
+    console.log(dataString)
+}
+
+let age = 15
+
+// less than 6 years old -> free
+// 6 to 17 years old     -> child discount
+// 18 to 26 years old    -> student discount
+// 27 to 66 years old    -> full price
+// over 66 years old     -> senior citizen discount
+
+// Create a conditional statement (if/else/else if) that logs out the discount
+// the passenger will get based upon the value of the age variable
+
+if (age < 6) {
+    console.log("Free")
+} else if (age < 18) {
+    console.log("Child Discount")
+} else if (age < 26) {
+    console.log("Student Discount")
+} else if (age < 66) {
+    console.log("Full Price")
+} else {
+    console.log("Senior citizen Discount")
+}
+
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+console.log("The 5 largest countries in the world:")
+for (let i = 0; i < largeCountries.length; i++)  {
+    console.log("- " + largeCountries[i])
+}
+
+let moreLargeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+moreLargeCountries.pop()
+moreLargeCountries.push("Pakistan")
+
+moreLargeCountries.shift()
+moreLargeCountries.unshift("China")
+
+////////////
+let dayOfMonth = 13
+let weekday = "Friday"
+
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
+
+if (weekday === "Friday" && dayOfMonth === 13) {
+    console.log("😱")
+}
+
+let hands = ["rock", "paper", "scissor"]
+
+// Create a function that returns a random item from the array
+
+function getHand(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length)
+    let item = arr[randomIndex]
+    return item
+}
+
+let choice = getHand(hands)
+console.log(choice)
